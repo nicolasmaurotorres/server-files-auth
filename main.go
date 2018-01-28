549698da71e38aff7b115cf499ddb34065db769d
maro <torres.nicolas.mauro@gmail.com>
2018-01-28 12:38:35 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+/* The person Type (more like an object)
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}*/
+/*type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+*/
+/* Display all from the people var
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}*/
+
+/* Display a single data
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+*/
+
+/* create a new item
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+*/
+
+/* Delete an item
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
+*/
+// main function to boot up everything
+func main() {
+	router := mux.NewRouter()
+
+	//Without login
+	router.HandleFunc("/", GetPeople).Methods("GET")
+	router.HandleFunc("/admin", GetPeople).Methods("GET")
+	router.HandleFunc("/admin/{user}/pass/{pass}").Methods("POST")
+
+	//Authentificated as ADMIN
+	router.HandleFunc("/admin/lobby/{auth}", GetPeople).Methods("GET")
+	router.HandleFunc("/admin/add/{auth}", AddUser).Methods("POST")
+	router.HandleFunc("/admin/del/{auth}", DelUser).Methods("POST")
+	router.HandleFunc("/admin/edit/{auth}", EditUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8001", router))
+}
